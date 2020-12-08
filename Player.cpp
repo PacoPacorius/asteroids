c890016704d685743c20b36e019e70ede2682a8c
Christos-Marios Perdikis <cperdikis@office365.auth.gr>
2020-12-08 05:06:41 +0200

    screen wrapping bitch

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,8 @@
 #include "Player.h"
 
+#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 768
+
 Player::Player() : Entity() { rotation = 0.f; }
 Player::Player(sf::Texture player_tex) : Entity(player_tex) { rotation = 0.f; }
 
@@ -8,12 +11,30 @@ Player::Player(sf::Texture player_tex, sf::Vector2f in_position) : Entity(player
 }
 
 void Player::input() {
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-		spr.move(sf::Vector2f(-5.f, 0.f));
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-		spr.move(sf::Vector2f(5.f, 0.f));
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-		spr.move(sf::Vector2f(0.f, -5.f));
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-		spr.move(sf::Vector2f(0.f, 5.f));
+	if (position.x > WINDOW_WIDTH + 10)
+		spr.setPosition(sf::Vector2f(-10, position.y));
+	else if (position.x < -10)
+		spr.setPosition(sf::Vector2f(WINDOW_WIDTH + 10, position.y));
+
+	if (position.y > WINDOW_HEIGHT + 10)
+		spr.setPosition(sf::Vector2f(position.x, -10));
+	else if (position.y < -10)
+		spr.setPosition(sf::Vector2f(position.x, WINDOW_HEIGHT + 10));
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+		spr.move(sf::Vector2f(-.5f, 0.f));
+		position = spr.getPosition();
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+		spr.move(sf::Vector2f(.5f, 0.f));
+	}
+		position = spr.getPosition();
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+		spr.move(sf::Vector2f(0.f, -.5f));
+		position = spr.getPosition();
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+		spr.move(sf::Vector2f(0.f, .5f));
+		position = spr.getPosition();
+	}
 }
