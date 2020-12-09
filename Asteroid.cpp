a7f78c64c3442e4f9950814492a2f99642fcb1ed
Panos Sysk <Pansysk75@gmail.com>
2020-12-10 00:06:11 +0200

    Initialize collision radius for Asteroids/Bullets depending on texture dimensions

--- Asteroid.cpp
+++ Asteroid.cpp
@@ -1,7 +1,9 @@
 #include "Asteroid.h"
 
 Asteroid::Asteroid() : Entity() {}
-Asteroid::Asteroid(sf::Texture tex) : Entity(tex) {}
+Asteroid::Asteroid(sf::Texture tex) : Entity(tex) { 
+	collision_circle.setRadius(tex.getSize().x / 2); 
+}
 
 Asteroid::Asteroid(sf::Texture tex, sf::Vector2f in_position, sf::Vector2f in_velocity) : Entity(tex) {
 	position = in_position;
