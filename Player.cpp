8d778a3b21575aa8ae3303bb3d1e4dad3e02f764
Christos-Marios Perdikis <c.perdikis@yahoo.gr>
2020-12-10 03:31:49 +0200

    movement is fuckin done lets fuckin go!

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,6 @@
 #define _USE_MATH_DEFINES
 
+#include <iostream>
 #include <cmath>
 #include "Player.h"
 
@@ -22,28 +23,33 @@ void Player::movement(float dt) {
 	float absolute_velocity = 0;
 	float radians = 0.f;
 
-	//if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-	//	spr.rotate(-ROTATION_SPEED);
-	//	//velocity += sf::Vector2f(-ACCELERATION * dt, 0.f);
-	//}
-	//if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-	//	spr.rotate(ROTATION_SPEED);
-	//	//velocity += sf::Vector2f(ACCELERATION * dt, 0.f);
-	//}
-	//if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && absolute_velocity < TERMINAL_VELOCITY) {
-	//	absolute_velocity = ACCELERATION * dt;
-	//}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+		spr.rotate(-ROTATION_SPEED);
+		//velocity += sf::Vector2f(-ACCELERATION * dt, 0.f);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+		spr.rotate(ROTATION_SPEED);
+		//velocity += sf::Vector2f(ACCELERATION * dt, 0.f);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && ACCELERATION * dt< TERMINAL_VELOCITY) {
+		absolute_velocity = ACCELERATION * dt;
+	}
 	/*if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && velocity.y < TERMINAL_VELOCITY) {
 		velocity += sf::Vector2f(0.f, ACCELERATION * dt);
 	}*/
 
-	spr.rotate(ROTATION_SPEED);
-	if(absolute_velocity < TERMINAL_VELOCITY) absolute_velocity = ACCELERATION * dt;
+	/*spr.rotate(ROTATION_SPEED);
+	if(ACCELERATION * dt < TERMINAL_VELOCITY) absolute_velocity = ACCELERATION * dt;*/
+
+	std::cout << "x: " << velocity.x << std::endl << "y: " << velocity.y << std::endl;
 
 	rotation = spr.getRotation();
 	radians = degrees_to_radians(rotation);
 
 	velocity += sf::Vector2f(absolute_velocity * sin(radians), -absolute_velocity * cos(radians));
+	if (pow(velocity.x, 2) + pow(velocity.y, 2) > TERMINAL_VELOCITY * 50) velocity -= sf::Vector2f(absolute_velocity * sin(radians), -absolute_velocity * cos(radians));
+	
+	
 	spr.move(dt * velocity);
 	position = spr.getPosition();
 }
