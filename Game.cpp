b4357b8d05aed983b08e37ae753454b19bc71792
Christos-Marios Perdikis <c.perdikis@yahoo.gr>
2020-12-11 01:21:34 +0200

    im a man with a plan

--- Game.cpp
+++ Game.cpp
@@ -50,6 +50,25 @@ void Game::create_asteroid(unsigned int number) {
 	for (unsigned int i = 0; i < number; i++) {
 		Asteroid asteroid(textures.asteroid);
 
+		/*	
+		*	* * * * * * * * * * * * * *
+		*	* HERE'S THE PLAN MY DUDE *
+		*	* * * * * * * * * * * * * *
+		* 
+		*  1. generate random numbers 17 - 49 (17 because of the origin point, 49 because anything more than 50 off the edge will get immediately deleted
+		* 
+		*  2. generate another set of numbers 0 - 3 which will determine on which side of the screen the asteroids will spawn
+		* 
+		*  3. depending on the side of the screen subtract from zero or add the numbers to the window dimensions
+		* 
+		*  4. generate a random angle at which the asteroids will fly towards (will take into account which side of the screen the asteroid is)
+		*  NOTE: I should probs test out which angles mean which directions, I know that degrees are clockwise but does up mean -45 to 45 or 0 to 90?
+		* 
+		*  5. give the asteroids a velocity using the code from the bullets and ship movement
+		* 
+		*  6. profit
+		*/
+
 		asteroid.set_velocity({ (std::rand() % 1000) / 10.f, (std::rand() % 1000) / 10.f });		// sets a random velocity for each of the asteroids
 		asteroids.push_back(asteroid);
 	}
